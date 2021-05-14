a2730964a98a77492750c96960726e5b94e55ec5
David Welles <74557848+davidwelles@users.noreply.github.com>
2021-05-14 16:58:40 -0400

    commit with homework

---
+++ week13day5/src/main/java/com/tts/week13day5/model/Subscriber.java
@@ -0,0 +1,98 @@
+package com.tts.week13day5.model;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+
+
+@Entity
+public class Subscriber {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    @Column
+    @CreationTimestamp
+    private Date signedUp;
+
+
+
+    public Subscriber(String firstName, String lastName, String userName, Date signedUp) {
+
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.signedUp = signedUp;
+    }
+
+    public Subscriber() {}
+
+    public Long getId() {
+        return id;
+    }
+
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Date getSignedUp() {
+        return signedUp;
+    }
+
+    public void setSignedUp(Date signedUp) {
+        this.signedUp = signedUp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Subscriber)) return false;
+        Subscriber that = (Subscriber) o;
+        return Objects.equals(getId(), that.getId()) && Objects.equals(getFirstName(), that.getFirstName()) && Objects.equals(getLastName(), that.getLastName()) && Objects.equals(getUserName(), that.getUserName()) && Objects.equals(getSignedUp(), that.getSignedUp());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFirstName(), getLastName(), getUserName(), getSignedUp());
+    }
+
+    @Override
+    public String toString() {
+        return "Subscriber{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", userName='" + userName + '\'' +
+                ", signedUp=" + signedUp +
+                '}';
+    }
+}
